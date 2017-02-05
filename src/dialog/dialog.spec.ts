8923700d015c57f5c21ea7b649dcf40613dd2ce1
lordfriend <lordfriend@outlook.com>
2017-02-05 18:09:02 +0800

    add new expect statment, remove Chrome, Firefox in karma.conf.js Browser section

--- src/dialog/dialog.spec.ts
+++ src/dialog/dialog.spec.ts
@@ -3,9 +3,11 @@ import {UIDialog} from './dialog';
 import {Component, NgModule} from '@angular/core';
 import {DialogRef} from './dialog-ref';
 import {UIDialogModule} from './index';
+import {DialogContainer} from './dialog-container';
 
 describe('UIDialog', () => {
     let dialog: UIDialog;
+    let dialogContainer: DialogContainer;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -15,14 +17,16 @@ describe('UIDialog', () => {
         TestBed.compileComponents();
     }));
 
-    beforeEach(inject([UIDialog], (d: UIDialog) => {
+    beforeEach(inject([UIDialog, DialogContainer], (d: UIDialog, dc: DialogContainer) => {
         dialog = d;
+        dialogContainer = dc;
     }));
 
     it('should open a dialog with given component', () => {
         let dialogRef = dialog.open(ExampleDialog, {stickyDialog: false});
-
+        let containerElement = dialogContainer.getContainerElement();
         expect(dialogRef.componentInstance).toEqual(jasmine.any(ExampleDialog));
+        expect(containerElement.querySelector('example-dialog')).not.toBe(null);
     });
 });
 
