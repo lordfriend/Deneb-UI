1ce98d9efbafdaa9799b62d20113c2be2adade8a
lordfriend <lordfriend@outlook.com>
2017-03-25 16:57:18 +0800

    add more test case in example

--- demo-app/app/infinite-list/infinite-list.component.ts
+++ demo-app/app/infinite-list/infinite-list.component.ts
@@ -8,17 +8,6 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
             height: 300px;
             display: block;
         }
-        .outer {
-            position: relative;
-            overflow-y: auto;
-            width: 100%;
-            height: 300px;
-        }
-        .inner {
-            position: relative;
-            width: 100%;
-            height: 950px;
-        }
     `]
 })
 export class InfiniteListDemo implements OnInit {
@@ -31,16 +20,16 @@ export class InfiniteListDemo implements OnInit {
             this.collection.push(i);
         }
 
-        // setTimeout(() => {
-        //     this.collection.splice(6, 1);
-        //     console.log('current collection', this.collection);
-        // }, 3000);
-        //
-        // setTimeout(() => {
-        //     this.collection.splice(2,  0, this.collection.splice(6, 1)[0]);
-        //     console.log('current collection', this.collection);
-        //     this.collection.splice(6, 1);
-        //     console.log('current collection', this.collection);
-        // }, 6000);
+        setTimeout(() => {
+            this.collection.splice(6, 1);
+            console.log('current collection', this.collection);
+        }, 3000);
+
+        setTimeout(() => {
+            this.collection.splice(2,  0, this.collection.splice(6, 1)[0]);
+            console.log('current collection', this.collection);
+            this.collection.splice(6, 1);
+            console.log('current collection', this.collection);
+        }, 6000);
     }
 }
