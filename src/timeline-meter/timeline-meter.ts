ad08d524924c824f899208d74583f1e5ddf696d7
lordfriend <lordfriend@outlook.com>
2017-03-29 19:01:12 +0800

    change ability to show label, limit to static label span

--- src/timeline-meter/timeline-meter.ts
+++ src/timeline-meter/timeline-meter.ts
@@ -3,6 +3,20 @@ import {
     ViewChild
 } from '@angular/core';
 import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+
+
+export class RowItem {
+    // use native Date instead Momentjs to get a good performance
+    // https://jsperf.com/moment-js-vs-native-date
+    date: Date;
+    rowHeight: number;
+}
+
+export class Marker {
+    labelSpan: 'string';
+    items: RowItem[];
+}
+
 @Component({
     selector: 'ui-timeline-meter',
     templateUrl: 'timeline-meter.html',
@@ -14,7 +28,12 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
 
     private _scrollPosition = new BehaviorSubject<number>(0);
 
-    private _rowHeightList: number[];
+    private _itemList: RowItem[];
+
+    private _meterWidth: number;
+    private _meterHeight: number;
+
+    marker: Marker[];
 
     @ViewChild('meter') meter: ElementRef;
 
@@ -22,20 +41,26 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
     timestampList: number[];
 
     /**
-     * minLabelSpan is used to regular what span of time should a label be displayed.
+     * labelSpan is used to regular what span of time should a label be displayed.
      * The labels display may vary base on timestamp list, height of each row and height of meter.
      * But the minimal time span of the label is not less than this value.
      * @type {string}
      */
     @Input()
-    minLabelSpan: 'year' | 'season' | 'month' | 'week' | 'day' | 'hour' = 'season';
+    labelSpan: 'year' | 'season' | 'month' | 'week' | 'day' | 'hour' = 'season';
+
+    @Input()
+    labelDateFormat: string;
 
     /**
-     * minMarkSpan should always smaller than minLabelSpan
+     * markSpan should always smaller than labelSpan
      * @type {string}
      */
     @Input()
-    minMarkSpan: 'season' | 'month' | 'week' | 'day' | 'hour' = 'week';
+    markSpan: 'season' | 'month' | 'week' | 'day' | 'hour' = 'month';
+
+    @Input()
+    showMarker: boolean = true;
 
     /**
      * if _rowHeight is set, meter will use this height for all rows.
@@ -47,7 +72,14 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
     rowHeight: number;
 
     set rowHeightList(list: number[]) {
-        this._rowHeightList = list;
+        this._itemList = list.map((rowHeight, index) => {
+            let item = new RowItem();
+            item.rowHeight = rowHeight;
+            if (this.timestampList && this.timestampList[index]) {
+                item.date = new Date(this.timestampList[index]);
+            }
+            return item;
+        });
         this.buildMeter();
     }
 
@@ -108,6 +140,18 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
                     }
                 )
         );
+
+        if (window) {
+            this._subscription.add(Observable.fromEvent(window, 'resize')
+                .debounceTime(300)
+                .subscribe(
+                    () => {
+                        let rect = this.meter.nativeElement.getBoundingClientRect();
+                        this._meterWidth = rect.width;
+                        this._meterHeight = rect.height;
+                    }
+                ));
+        }
     }
 
     ngOnDestroy(): void {
@@ -115,6 +159,14 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
+        if ('timestampList' in changes && !this.rowHeight && this._itemList) {
+            let currentTimestampList = changes['timestampList'].currentValue;
+            if (currentTimestampList.length === this._itemList.length) {
+                this._itemList.forEach((item, index) => {
+                    item.date = new Date(currentTimestampList[index]);
+                });
+            }
+        }
         if ('timestampList' in changes || 'rowHeight' in changes) {
             this.buildMeter();
         }
@@ -122,12 +174,35 @@ export class UITimeLineMeter implements AfterViewInit, OnDestroy, OnChanges {
 
     private buildMeter() {
         if (this.rowHeight && this.timestampList) {
-            this._rowHeightList = [];
-            for (let i = 0; i< this.timestampList.length; i++) {
-                this._rowHeightList.push(this.rowHeight);
-            }
+            this._itemList = [];
+            this._itemList = this.timestampList.map((timestamp) => {
+                let item = new RowItem();
+                item.date = new Date(timestamp);
+                item.rowHeight = this.rowHeight;
+                return item;
+            });
         }
+        this.marker = [];
         // need an algorithm to build meter
+        let firstItem = this._itemList[0];
+        let lastItem = this._itemList[this._itemList.length - 1];
+        let labelSpan;
+        // if (firstItem.date.getFullYear() === lastItem.date.getFullYear()) {
+        //     if (Math.abs(firstItem.date.getMonth() - lastItem.date.getMonth()) <= 3) {
+        //         if (firstItem.date.getMonth() === lastItem.date.getMonth()) {
+        //             if (firstItem.date.getMonth())
+        //         } else {
+        //             labelSpan = 'month';
+        //         }
+        //     } else {
+        //         // same year but different season. use season as label
+        //         labelSpan = 'season';
+        //     }
+        // } else {
+        //     labelSpan = 'year';
+        // }
+        let i = 1;
+        let lastGroupStart = this._itemList[0];
     }
 
     private scrollTo(pos: number) {
