adb30786511f438618789ee6083a42b756f486ce
lordfriend <lordfriend@outlook.com>
2017-04-01 16:35:15 +0800

    fix pagination bugs to support aot

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -1,7 +1,7 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 
-interface PageNumber {
+export interface PageNumber {
     number: number,
     text: string,
     active: boolean
