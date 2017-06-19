911f9972afe01b96244b4dc16412dcc47f97aefb
lordfriend <lordfriend@outlook.com>
2017-06-19 10:04:36 +0800

    update version, update shell script

--- publish.sh
+++ publish.sh
@@ -2,7 +2,12 @@ if [ $(npm whoami) != 'lordfriend' ]; then
     echo 'use invalid'
     exit 1;
 fi
-echo 'user valid, run test'
+
+if [ $(git describe --contains --all HEAD) != 'master' ]; then
+    echo 'publish must be done at master branch'
+    exit 1;
+fi
+echo 'user and branch valid, run test'
 # unit test
 npm test
 
