ec2fd690cc525107c12cb9b1765af5c1e056d60c
lordfriend <lordfriend@outlook.com>
2017-06-19 09:54:42 +0800

    remove ChromeTravisCI browser

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function (config) {
     let browsers = ['Chrome', 'PhantomJS'];
     if (process.env.TRAVIS) {
         browsers = [
-            'ChromeTravisCI',
             'PhantomJS'
         ];
     }
